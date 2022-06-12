c6f19b8b6f66cca85bff86e5129926adc2f909ab
David McConnell <mccodav1@gmail.com>
2022-06-12 15:40:24 -0400

    Initial Commit
    Adds src files from instructor to project / package
    Adds Searcher / Sorter classes

---
+++ src/proj3/Searcher.java
@@ -0,0 +1,5 @@
+package proj3;
+
+public class Searcher {
+	// implement binary search algorithm: iterative (loop) or recursive
+}
---
+++ src/proj3/Sorter.java
@@ -0,0 +1,5 @@
+package proj3;
+
+public class Sorter {
+	// implement quicksort algorithm
+}
